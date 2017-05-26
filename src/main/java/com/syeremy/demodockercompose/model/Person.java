85545e8095c661deabfff21d6dfa5a3a1762ea6e
syeremy <syeremy@gmail.com>
2017-05-25 20:15:53 -0500

    init-2

--- src/main/java/com/syeremy/demodockercompose/model/Person.java
+++ src/main/java/com/syeremy/demodockercompose/model/Person.java
@@ -1,7 +1,50 @@
 package com.syeremy.demodockercompose.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
 /**
  * Created by syeremy on 5/25/17.
  */
+@Entity
+@Table(name = "people")
 public class Person {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    @Column(updatable = false)
+    private String id;
+
+    @Version
+    private Long version;
+
+    private String name;
+
+    private String email;
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
